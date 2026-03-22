2d048464d7e50c0dcf243c7122dbe4c5b37b1423
mayaralharazi <malharaz@charlotte.edu>
2026-03-22 16:38:50 -0400

    Update introduction.js

--- itis3135/scripts/introduction.js
+++ itis3135/scripts/introduction.js
@@ -1,1 +1,35 @@
+const form = document.getElementById("form");
 
+// prevent refresh
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const firstName = document.getElementById("firstName").value;
+    const middleName = document.getElementById("middleName").value;
+    const nickname = document.getElementById("nickname").value;
+    const lastName = document.getElementById("lastName").value;
+    const adjective = document.getElementById("adjective").value;
+    const animal = document.getElementById("animal").value;
+    const divider = document.getElementById("divider").value;
+    const caption = document.getElementById("caption").value;
+    const statement = document.getElementById("statement").value;
+
+    document.body.innerHTML = `
+        <h2>Introduction Form</h2>
+        <h3>${firstName} ${middleName} "${nickname}" ${lastName} ${divider} ${adjective} ${animal}</h3>
+
+        <figure>
+            <img src="images/yourphoto.jpg" width="200">
+            <figcaption>${caption}</figcaption>
+        </figure>
+
+        <p>${statement}</p>
+
+        <br><a href="intro_form.html">Reset</a>
+    `;
+});
+
+// clear button
+document.getElementById("clearBtn").addEventListener("click", function () {
+    document.querySelectorAll("form input, form textarea").forEach(input => input.value = "");
+});
