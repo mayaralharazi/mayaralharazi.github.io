71e4a2f133dafecab2ce9a5cce8bb6630055b07f
mayaralharazi <malharaz@charlotte.edu>
2026-04-22 21:19:24 -0400

    Create top.js

---
+++ itis3135/project/scripts/top.js
@@ -0,0 +1,16 @@
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+topBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
