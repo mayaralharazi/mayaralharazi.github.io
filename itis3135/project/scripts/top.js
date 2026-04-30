3f96831073b63ae110f003c589f55c47ea5741f0
mayaralharazi <malharaz@charlotte.edu>
2026-04-30 16:55:22 -0400

    Update top.js

--- itis3135/project/scripts/top.js
+++ itis3135/project/scripts/top.js
@@ -14,3 +14,15 @@ topBtn.onclick = function () {
     behavior: "smooth"
   });
 };
+
+const currentPage = window.location.pathname.split("/").pop();
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  const linkPage = link.getAttribute("href");
+
+  if (linkPage === currentPage) {
+    link.classList.add("active");
+  }
+});
